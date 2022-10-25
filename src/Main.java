a302d358c490fdadeba2b661a0747788c693fd66
vadym <vadikhide@yandex.ru>
2022-10-25 16:56:53 +0300

    Методы на проверку С учетом оптимизации кода. НО! Ребята, не пишите тогда, что можно использовать решение из предыдущих уроков. В предыдущих уроках как раз было требования на создание лишних переменных

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void verifyHighYear(int year) {
 
-        if (year % 4 == 0 && year % 100 != 0) {
+        if (year % 4 == 0) {
             System.out.println(year + " год является високосным");
-        } else if (year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else if (year % 100 == 0) {
-            System.out.println(year + " год не является високосным");
-        } else if (year % 4 != 0) {
+        } else {
             System.out.println(year + " год не является високосным");
         }
     }
